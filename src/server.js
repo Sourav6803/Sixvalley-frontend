e955d942ee405bfd24dba0589f21f60518e97716
Sourav Bhukta <souravbhukta8@gmail.com>
2024-09-01 18:34:13 +0530

    vv

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@
 
 
 
-export const server = "https://sixvalley-backend.onrender.com/api/v2"
+export const server = "https://e-backend-136t.onrender.com/api/v2"
 export const backend_url = "https://e-backend-136t.onrender.com/"
 
 // git push -u origin main
