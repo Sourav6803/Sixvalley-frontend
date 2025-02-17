a76afe6996febf73069bec3a53fc6c6ffdb84171
Sourav6803 <souravbhukta8@gmail.com>
2025-02-17 19:16:58 +0530

    category api update

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v3/category";
+// const BASE_URL = "http://localhost:8000/api/v3/category";
+const BASE_URL = "https://e-backend-136t.onrender.com/api/v3"
 
 const config = { 
     headers: { "Content-Type": "multipart/form-data" },
