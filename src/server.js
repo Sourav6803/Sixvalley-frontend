48b899c6f55b754b041effff243ea6129c7a4d81
Sourav7928 <Sourav.bhukta@infomaticae.co.in>
2025-08-29 09:28:02 +0530

    server update

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 
 
- export const server = "http://localhost:8000/api/v2"
+//  export const server = "http://localhost:8000/api/v2"
 
- export const backend_url = "http://localhost:8000/"
+//  export const backend_url = "http://localhost:8000/"
 
- export const serverTwo = "http://localhost:8000/api/v3"
+//  export const serverTwo = "http://localhost:8000/api/v3"
 
 
-// export const server = "https://e-backend-136t.onrender.com/api/v2"
-// export const backend_url = "https://e-backend-136t.onrender.com/"
-// export const serverTwo = "https://e-backend-136t.onrender.com/api/v3"
+export const server = "https://e-backend-136t.onrender.com/api/v2"
+export const backend_url = "https://e-backend-136t.onrender.com/"
+export const serverTwo = "https://e-backend-136t.onrender.com/api/v3"
 
 // git push -u origin main
 
